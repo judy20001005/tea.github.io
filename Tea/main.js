4c158ccfd0f645ce505f982d041516fd701d001d
judy20001005 <94591774+judy20001005@users.noreply.github.com>
2024-09-22 13:40:41 +0800

    version2

---
+++ Tea/main.js
@@ -0,0 +1,13 @@
+//導覽列
+let ChangeIcon = function(icon){
+    icon.classList.toggle('fa-xmark')
+}
+
+const header = document.querySelector('.header')
+const toggle = document.querySelector('#toggle');
+
+toggle.addEventListener('click',() => {
+    header.classList.toggle('show-nav');
+})
+
+
